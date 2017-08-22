b0f9872ed28a509c71468bd46db97997947808f6
Julien Vicini <julien@fakedomain.fakeextension>
2017-08-23 00:33:02 +0200

    Add QueryBuilder.Core.Tests Project

--- src/QueryBuilder.Core/Helpers/ThrowHelper.cs
+++ src/QueryBuilder.Core/Helpers/ThrowHelper.cs
@@ -6,22 +6,20 @@ namespace QueryBuilder.Helpers
 {
     public static class ThrowHelper
     {
-        public static void ThrowIfNullOrEmpty<T>(IEnumerable<T> enumerable, string argumentName)
-        {
-            if (string.IsNullOrWhiteSpace(argumentName))
-                throw new ArgumentException(nameof(argumentName));
-
-            if (enumerable == null || !enumerable.Any())
-                throw new ArgumentException($"The parameter \"{argumentName}\" must contains at least one item.");
-        }
-
         public static void ThrowIfNullOrWhiteSpace(string value, string argumentName)
         {
-            if (string.IsNullOrWhiteSpace(argumentName))
-                throw new ArgumentException(nameof(argumentName));
+            ThrowIfNullOrWhiteSpace(argumentName, nameof(argumentName));
 
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentException($"The parameter \"{argumentName}\" cannot be null or a white space.");
+        }
+
+        public static void ThrowIfNullOrEmpty<T>(IEnumerable<T> value, string argumentName)
+        {
+            ThrowIfNullOrWhiteSpace(argumentName, nameof(argumentName));
+
+            if (value == null || !value.Any())
+                throw new ArgumentException($"The parameter \"{argumentName}\" must contains at least one item.");
         }
     }
 }
---
+++ tests/QueryBuilder.Core.Tests/Helpers/ThrowHelperTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueryBuilder.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueryBuilder.Core.Tests.Helpers
+{
+    [TestClass]
+    public class ThrowHelperTests
+    {
+        #region ThrowIfNullOrWhiteSpace
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpaceThrowsArgumentExceptionWhenValueIsNull()
+        {
+            string value = null;
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrWhiteSpace(value, "parameter")
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpaceThrowsArgumentExceptionWhenValueIsWhiteSpace()
+        {
+            var value = "   ";
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrWhiteSpace(value, "parameter")
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpaceThrowsArgumentExceptionWhenParameterNameIsNull()
+        {
+            string paramterName = null;
+            var data = "NotEmpty";
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrWhiteSpace(data, paramterName)
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpaceThrowsArgumentExceptionWhenParameterNameIsWhiteSpace()
+        {
+            var paramterName = "   ";
+            var data = "NotEmpty";
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrEmpty(data, paramterName)
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpaceShouldNotThrowException()
+        {
+            var paramterName = "parameter";
+            var data = "NotEmpty";
+
+            ThrowHelper.ThrowIfNullOrEmpty(data, paramterName);
+        }
+
+        #endregion
+
+        #region ThrowIfNullOrEmpty
+
+        [TestMethod]
+        public void ThrowIfNullOrEmptyThrowsArgumentExceptionWhenValueIsNull()
+        {
+            IEnumerable<int> nullData = null;
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrEmpty(nullData, "parameter")
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmptyThrowsArgumentExceptionWhenValueIsEmpty()
+        {
+            var emptyData = Enumerable.Empty<int>();
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrEmpty(emptyData, "parameter")
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmptyThrowsArgumentExceptionWhenParameterNameIsNull()
+        {
+            string paramterName = null;
+            var data = new List<int>() { 1 };
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrEmpty(data, paramterName)
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmptyThrowsArgumentExceptionWhenParameterNameIsWhiteSpace()
+        {
+            string paramterName = "   ";
+            var data = new List<int>() { 1 };
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                ThrowHelper.ThrowIfNullOrEmpty(data, paramterName)
+            );
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmptyShouldNotThrowException()
+        {
+            string paramterName = "parameter";
+            var data = new List<int>() { 1 };
+
+            ThrowHelper.ThrowIfNullOrEmpty(data, paramterName);
+        }
+
+        #endregion
+    }
+}
