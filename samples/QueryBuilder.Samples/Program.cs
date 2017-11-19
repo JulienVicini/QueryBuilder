06ff25e739f007e5cf09033efaea2ccd1e5cf43b
julien <julien@dev>
2017-11-19 15:35:39 +0100

    - Upgrade nugget packages - make sample code more readable

--- samples/QueryBuilder.Samples/Program.cs
+++ samples/QueryBuilder.Samples/Program.cs
@@ -9,46 +9,43 @@ namespace QueryBuilder.Samples
     {
         static void Main(string[] args)
         {
-            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
-
-            IEnumerable<Person> persons
-                = Enumerable.Range(0, 100)
-                            .Select(index => new Person()
-                            {
-                                Age       = index,
-                                FirstName = $"FirstName n°{index}",
-                                LastName  = $"LastName n°{index}"
-                            })
-                            .ToList();
-
             using(var context = new SchoolDbContext())
             {
                 // Delete
                 context.Persons.Delete();
 
                 // Bulk Insert
-                context.Persons.BulkInsert(persons);
+                IEnumerable<Person> newPersons = CreatePersons(count: 100); 
+                context.Persons.BulkInsert(newPersons);
 
                 // Bulk Merge
-                var dbPersons = context.Persons.AsNoTracking()
-                                               .Take(1000)
-                                               .ToList();
+                IEnumerable<Person> dbPersons = context.Persons.AsNoTracking()
+                                                       .Take(1000)
+                                                       .ToList();
 
-                dbPersons.ForEach(p => {
-                    p.Age += 10;
-                });
+                foreach(Person person in dbPersons)
+                    person.Age += 10;
 
                 context.Persons.BulkMerge(dbPersons, p => new { p.FirstName, p.LastName });
 
-                // Or Update
-                context.Persons.Where(p => p.Id%2 == 0)
+                // Update
+                context.Persons.Where(p => p.Id % 2 == 0)
                                .SetValue(p => p.Age, 0)
+                               // TODO Do not work so far .SetValue(p => p.Age, p => p.Age / p.Id)
                                .Update();
             }
+        }
 
-            stopWatch.Stop();
-
-            System.Console.WriteLine($"The program un in {stopWatch.ElapsedMilliseconds}ms.");
+        protected static IEnumerable<Person> CreatePersons(int count)
+        {
+            for(int i =0; i < count; i++)
+            {
+                yield return new Person() {
+                    Age       = i,
+                    FirstName = $"FirstName n°{i}",
+                    LastName  = $"LastName n°{i}"
+                };
+            }
         }
     }
 }
--- src/QueryBuilder.Core/Helpers/ThrowHelper.cs
+++ src/QueryBuilder.Core/Helpers/ThrowHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace QueryBuilder.Helpers
 {
+    [DebuggerStepThrough]
     public static class ThrowHelper
     {
         public static void ThrowIfNull<T>(T value, string argumentName)
