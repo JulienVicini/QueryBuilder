14ce7c320fd10844f66cf0550e0beca4caf36cf4
julien <julien@dev>
2018-02-01 23:21:09 +0100

    Add classes to implement
    Related Work Items: #29, #30

---
+++ src/QueryBuilder.EFCore/Database/EFCoreContextAdapter.cs
@@ -0,0 +1,29 @@
+﻿using QueryBuilder.Core.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+namespace QueryBuilder.EntityFramework.Database
+{
+    public class EFCoreContextAdapter<TConnection, TTransaction>
+        : IDatabaseContext<TConnection, TTransaction>, ICommandProcessing
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+    {
+        public TTransaction BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteCommand(string query, IEnumerable<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TConnection GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/QueryBuilder.EFCore/Mappings/EFCoreMappingAdapter.cs
@@ -0,0 +1,23 @@
+﻿using QueryBuilder.Core.Mappings;
+using System;
+using System.Collections.Generic;
+
+namespace QueryBuilder.EntityFramework.Mappings
+{
+    public class EFCoreMappingAdapter<TEntity>
+        : IMappingAdapter<TEntity>
+        where TEntity : class
+    {
+        public Type EntityType => throw new NotImplementedException();
+
+        public IEnumerable<ColumnMapping<TEntity>> GetColumns()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTableName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
