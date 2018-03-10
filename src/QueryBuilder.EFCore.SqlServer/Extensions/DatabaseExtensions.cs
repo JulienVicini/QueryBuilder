976b8331df50faf32de0e9d6e8335e7eddf07c8b
julien <julien@dev>
2018-03-10 02:35:43 +0100

    Fix QueryHelper.GetDbContext

---
+++ src/QueryBuilder.EFCore.SqlServer/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace QueryBuilder.EFCore.SqlServer.Extensions
+{
+    using System.Reflection;
+    using Microsoft.EntityFrameworkCore.Storage;
+    using QueryBuilder.Core.Helpers;
+
+    public static class DatabaseExtensions
+    {
+        private static readonly PropertyInfo Dependencies_Property = typeof(Database).GetProperty("Dependencies", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static DatabaseDependencies GetDependencies(this Database database)
+        {
+            ThrowHelper.ThrowIfNull(database, nameof(database));
+
+            return (DatabaseDependencies)Dependencies_Property.GetValue(database);
+        }
+    }
+}
---
+++ src/QueryBuilder.EFCore.SqlServer/Extensions/EntityQueryProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using QueryBuilder.Core.Helpers;
+
+namespace QueryBuilder.EFCore.SqlServer.Extensions
+{
+    public static class EntityQueryProviderExtensions
+    {
+        private static readonly FieldInfo QueryCompiler_FieldInfo = typeof(EntityQueryProvider).GetField("_queryCompiler", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static QueryCompiler GetQueryCompiler(this EntityQueryProvider entityQueryProvider)
+        {
+            ThrowHelper.ThrowIfNull(entityQueryProvider, nameof(entityQueryProvider));
+
+            return (QueryCompiler)QueryCompiler_FieldInfo.GetValue(entityQueryProvider);
+        }
+    }
+}
